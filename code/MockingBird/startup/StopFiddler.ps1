2fabc0be11752b93cd440940b5aee2e55db21bb9
Hemant Sharma <hemant.kularia@gmail.com>
2022-06-25 16:06:34 +0530

    Issue #12 updated csproj to include templates folder in out put and enhanced powershell script for easy debugging

---
+++ code/MockingBird/startup/StopFiddler.ps1
@@ -0,0 +1,11 @@
+﻿# get Firefox process
+$fiddlerInstance = Get-Process Fiddler -ErrorAction SilentlyContinue
+if ($fiddlerInstance) {
+  # try gracefully first
+  $fiddlerInstance.CloseMainWindow()
+  # kill after five seconds
+  Sleep 5
+  if (!$fiddlerInstance.HasExited) {
+    $fiddlerInstance | Stop-Process -Force
+  }
+}
